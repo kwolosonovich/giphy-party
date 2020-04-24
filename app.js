8bf955c715f253fd9ac019ad4a955614485da274
kelly wolosonovich <k.wolosonovich@gmail.com>
2020-04-24 00:56:09 -0700

    updated app.js code

--- app.js
+++ app.js
@@ -1,41 +1,107 @@
 console.log("Let's get this party started!");
 
-const apiKey = 'AFkYNpS3QrPyymUgOlzjhEcxIH04Q3sY';
+
+const newSearch = document.getElementById('search');
+const deleteGifs = document.getElementById("deleteBtn");
+const currentParty = document.getElementById("gallery");
+const deleteCheck = document.getElementById("delete-party");
 
 
-const newSearch = document.getElementById("#search");
+
 
 newSearch.addEventListener("click", function (e) {
-  e.preventDefault();
-  console.log("event");
+  e.preventDefault()
+  const input = document.querySelector('#keyword-seach').value
+  getGif(input)
+  
 });
 
-// const form = document.getElementById("#keyword-search");
-// form.addEventListener("submit", function (e) {
-//     e.preventDefault()
-//     console.log('at event')
-//     const input = document.querySelector('#keyword-seach')
-//     getGif(input.value)
-// })
+async function gitTrending() {
+    const res = axios.get(
+      "https://api.giphy.com/v1/gifs/trending?api_key=AFkYNpS3QrPyymUgOlzjhEcxIH04Q3sY&limit=20&rating=G");
+}
 
 async function getGif(keyword) {
-    try {
-        const url = `http://api.giphy.com/v1/gifs/search?q=${keyword} `;
-        const res = await axios.get(url);
-        createNewGif(res)
+    const api_key = "AFkYNpS3QrPyymUgOlzjhEcxIH04Q3sY";
+    const q=`${keyword}`
 
-    } catch (e) {
-        alert("Sorry, no gifs match your search. Please enter a different keyword.")
+    // const res = await axios.get('http//api.giphy.com/v1/gifs/search', {params: {api_key, q}})
+
+    // try {
+        const res = await axios.get("https://api.giphy.com/v1/gifs/search?api_key="+api_key+"&q="+q+"&limit=200&offset=0&rating=G&lang=en")
+        .then(function (response){
+            const ranGif = Math.floor(Math.random() * 200) +1
+            createNewGif(JSON.parse(response.request.response).data[ranGif].images["preview_webp"]["url"])
+        })
+            // console.log(JSON.parse(res.request.response))
+        // const res = await axios.get("https://api.giphy.com/v1/gifs/search?api_key="+api_key+"&q="+q)
+        // .then(function(response) {
+        //     console.log(response.data.url)
+        // })
+
+        //   console.log(res.value);
+            // console.log(res.data.message)
+            // console.log(res.headers)
+            // console.log(JSON.parse(this.responseText));
+            // console.log(JSON.parse(res));
+            // console.log()
+            // console.log(res.json())
+        //   let url = await JSON.parse(res)
+        //   let url = await res.data.url;
+        //   let url = res[0].url
+        //  let url = res.data.request.response.url
+        //   let url = res.data.reponse.url
+        //   console.log(url)
+
+        //   createNewGif(url)
+
+    // } catch (e) {
+    //     alert("Sorry, no gifs match your search. Please enter a different keyword.")
+    // }
+
+         // const res = await axios.get('http//api.giphy.com/v1/gifs/search', {params: {api_key, q}})
+
+        // const res = await axios.get(`"https//api.giphy.com/v1/gifs/search?api_key=${apiKey}&q=${keyword}"`);
+
+        // const url = `http://api.giphy.com/v1/gifs/search?q=${keyword} `;
+        // const res = await axios.get(url);
+        // console.log(url)
+        // createNewGif(res)
+
+}
+
+
+createNewGif = (gif) => {
+
+    const gifGallery = document.getElementById('gallery')
+    const newDiv = document.createElement('div')
+    console.log(newDiv)
+    newDiv.className += "col col-4 col-md-3"
+    const gifImg = document.createElement('img')
+    gifImg.src = gif;
+    gifImg.className += "img-thumbnail img-fluid";
+    newDiv.append(gifImg)
+    gifGallery.append(newDiv)
+}
+
+deleteGifs.addEventListener('click', function(e) {
+    e.preventDefault()
+    verifyDelete()    
+})
+
+const verifyDelete = () => {
+    if (deleteCheck.checked) {
+        deleteParty()
+    } else {
+        alert('To end your current party, please select the delete party checkbox')
     }
 }
 
-createNewGif = (gif) => {
-    const gifGallery = document.getElementById('gallery')
-    const newDiv = document.createElement('div')
-    newDiv.className += "col"
-    const gitImg = document.createElement('img')
-    gifImg.src = gif;
-    gifImg.className += "img-thumbnail"
-    newDiv.append(gifImg)
-    gifGallery.append(newDiv)
+const deleteParty = () => {
+    partyArr = []
+    var img = currentParty.firstElementChild
+    while (img) {
+        img.remove()
+        img = currentParty.firstElementChild
+    }
 }
